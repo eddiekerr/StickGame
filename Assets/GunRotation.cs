92da9d1b6cd54354fe664c5edfc4266d07067780
Edward Kerr <edward.kerr1995@gmail.com>
2017-10-04 23:12:03 +1300

    Initial commit of StickGame2d

---
+++ Assets/GunRotation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunRotation : MonoBehaviour {
+
+    public int rotationOffset = 90;
+    public Vector2 GetVector2;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        difference.Normalize();
+        
+        if(GetVector2 == Vector2.right)
+        {
+            float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, 0f, rotZ + rotationOffset);
+        }
+        else
+        {
+            float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, 0f, (rotZ + rotationOffset) * -1);
+        }
+        
+    }
+}
